f23ea94024482d3e8f9cb732dd3376bf50541887
zevifriedman <zevifriedmen@gmail.com>
2024-11-20 17:40:05 +0200

    1.06

---
+++ src/game/objects/coin/Coin.java
@@ -0,0 +1,32 @@
+package game.objects.coin;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Coin extends JPanel {
+    private ImageIcon gifIcon;
+
+    public Coin() {
+        setBounds(50, 50, 20, 20);
+        setOpaque(false);
+        loadGif();
+    }
+
+    public void loadGif() {
+        gifIcon = new ImageIcon("C:\\Users\\JBH\\Downloads\\gifmaker_me.gif");
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (gifIcon != null) {
+            gifIcon.paintIcon(this, g, 0, 0);
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(gifIcon.getIconWidth(), gifIcon.getIconHeight());
+    }
+}
